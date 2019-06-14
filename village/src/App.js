5169e3c72223545b0de60a828611827056702397
Anna W <anski.anna@gmail.com>
2019-06-14 08:23:16 +0100

    got all smurfs using GET requst

--- village/src/App.js
+++ village/src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+
+
+const smurfApi = 'http://localhost:3333/smurfs'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      errorMessage: '',
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+  componentDidMount(){
+    this.getAllSmurfs();
+  }
+
+  getAllSmurfs = () => {
+    axios
+     .get(smurfApi)
+     .then(response => {
+       this.setState({ smurfs: response.data })
+     })
+     .catch(error => {
+       this.setState({ errorMessage: error.message })
+     } )
+  }
+
+
+
   render() {
     return (
       <div className="App">
